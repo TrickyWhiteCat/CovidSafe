fc1e5942c010e5022e898290b76a163b1cf4e322
TrickyWhiteCat <nmttuan17@gmail.com>
2022-12-25 20:28:52 +0700

    Finish interference between board and solver

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
-import logging
+import random
+import time
 
 
-logging.basicConfig(filename='logger.log')
 class Solver:
     def __init__(self, path_to_board: str = None, path_to_command: str = None):
         """
@@ -14,33 +14,56 @@ class Solver:
         if path_to_command is None:
             path_to_command = "command.inp"
         self.command_path = path_to_command
-        self.iter = 1
+        self.__iter = 1
+        self.solved = False
+        self.__finished = False
 
-    def read_board(self):
+    def __read_board(self):
         path = self.board_path
         while True: # Wait for the file to be updated
             with open(path, mode='r') as board:
                 try:
                     iter = int(board.readline())
-                    if iter < self.iter:
+                    if iter < self.__iter:
                         board_state = [line.split(",") for line in board.readlines()]
-                        self.board_state = board_state
-                        logging.info("fine")
+                        self.__board_state = board_state
                         return
                 except ValueError:
                     pass
 
-    def write_command(self):
-        import random
+    def __write_command(self, row, col):
         with open(self.command_path, mode = 'w') as cmd:
-            #self.read_board()
-            cmd.write(f"{self.iter}\n")
-            cmd.write(f"{' '.join([str(val) for val in random.choices(range(1, 11), k = 2)])}")
-            self.iter += 1
+            cmd.write(f"{self.__iter}\n")
+            cmd.write(f"{row} {col}")
+            self.__iter += 1
             cmd.close()
 
+    def __choose_pos(self):
+        return random.randint(1, 10), random.randint(1, 10)
+
+    def __check_finished(self):
+        for row in self.__board_state:
+            if "V" in row: # Virus detected
+                self.solved = False
+                self.__finished = True
+                return
+        
+        for row in self.__board_state:
+            if " " in row: # There exists unopened cell
+                self.__finished = False
+                return
+
+        # No unopened cell left
+        self.__finished = True
+        self.solved = True
+
+
     def solve(self):
-        import time
-        while True:
-            time.sleep(1)
-            self.write_command()
+        while not self.__finished:
+            self.__read_board()
+            row, col = self.__choose_pos()
+            self.__write_command(row = row, col = col)
+            self.__check_finished()
+        
+        with open("result.txt", 'a') as res_file:
+            res_file.write(f"{int(self.solved)}\n")
